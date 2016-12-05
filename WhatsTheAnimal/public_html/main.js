1feb41c03edc1a3c4daa304249ae40044492ea5c
Albrethsen-Trevor <alb12014@byui.edu>
2016-12-05 15:56:24 -0800

    Included a Current Score and High Score counter that uses JSON to parse and stringify. Also stores scores in local storage. Player can write their first and last name so score is associated with their name. Made minor inclusions in CSS.

--- WhatsTheAnimal/public_html/main.js
+++ WhatsTheAnimal/public_html/main.js
@@ -1,1 +1,46 @@
+function storePlayerName(){
+    var firstName = document.getElementById('name1').value;
+    var lastName = document.getElementById('name2').value;
+    
+    var player = {
+        firstname: firstName,
+        lastname: lastName,
+    };
+    
+    var json = JSON.stringify(player);
+    if(window.localStorage){
+        localStorage.setItem('Player', json);
+    };
+}
 
+function load(){
+    var player = JSON.parse(localStorage.getItem('Player'));
+    console.log(player);
+    document.getElementById('name').innerHTML = player.firstname + " " + player.lastname;
+    if(window.localStorage){
+        highScore = localStorage.getItem('High Score');
+        currentScore = localStorage.getItem('Current Score');
+        document.getElementById('high_score').innerHTML = highScore;
+        document.getElementById('current_score').innerHTML = currentScore;
+    }
+}
+
+var currentScore = 0;
+var highScore;
+
+function getHighScore(){
+    if(window.localStorage){
+        highScore = localStorage.getItem('High Score');
+        document.getElementById('high_score').innerHTML = highScore;
+    }
+}
+
+function setHighScore(){
+    if(window.localStorage){
+        topScore = localStorage.getItem('High Score');
+        if(currentScore >= highScore){
+            localStorage.setItem('High Score', currentScore);
+            document.getElementById('high_score').innerHTML = currentScore;
+        }
+    }
+}
